5862e576aacd0383e6aefd6c81c36c8a218388ce
neomaraiya <neomaraiya@outlook.com>
2021-05-15 17:21:56 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component, VERSION } from '@angular/core';
 
+// 1. Create two text box - first name and last name
+// 2. Add a button "Add User".
+// 3. Add a list to display users
+// 4. Once user clicks on the "Add User", add the fullname to the list
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
