7f33df8827c4616aeced6cd4f24b584c62e4ac80
neomaraiya <neomaraiya@outlook.com>
2021-06-30 14:05:05 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component, VERSION } from '@angular/core';
 
 // 1. Create two text box - first name and last name
-// 2. Add a button "Add User".
-// 3. Add a list to display users
-// 4. Once user clicks on the "Add User", add the fullname to the list
+// 2. Focus the first text box on page load i.e first name
+// 3. Add a button "Add User".
+// 4. Add a list to display users
+// 5. Once user clicks on the "Add User", add the fullname to the list
 
 @Component({
   selector: 'my-app',
